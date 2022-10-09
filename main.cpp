6532f6a3dc837c4f75913dcb541e29282ff4e74b
Yehongxxu <49933863+Yehongxxu@users.noreply.github.com>
2022-10-09 12:31:12 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,243 +1,237 @@
-#include <iostream>
-
 #include "traffic/Traffic.h"
-#include "traffic/routingMain.cpp"
 #include "KSP/kspwlo.h"
-#include "baseline/KSPAlloc.h"
-#include <random>
-#include <chrono>
-#include <experimental/filesystem>
+#include "baseline/Baseline.h"
+#include <filesystem>
 
 using namespace std;
+namespace fs = std::filesystem;
 
+void writeRoutingResult(TrafficMaintain &traffic, const string &fileName) {
+    ofstream outFile(fileName);
+    const vector<vector<Label>> footprints = traffic.footprints;
 
-int getRandInt(int range)
-{
-    random_device rd; // obtain a random number from hardware
-    mt19937 gen(rd()); // seed the generator
-    uniform_int_distribution<> distr(0, range); // define the range
-    return distr(gen);
-}
+    for (int i = 0; i < traffic.reqNo; ++i) {
+        Timer clock;
+        long int sd = Framework::Dij(traffic, i);
+        clock.tock();
+        long int dijT = clock.duration().count();
 
-vector<NodeId> getNodesInR(Coord &center, int r, RoadNetwork &rN)
-{
-    vector<NodeId> v;
-    for (NodeId i = 0; i <= rN.numNodes; i++)
-    {
-        if (RoadNetwork::distance(center, rN.coords[i]) <= r)
-        {
-            v.emplace_back(i);
-        }
+        outFile << i << "|" << sd << "|" << dijT << "|" << traffic.requestODs[i].cost << "|";
+//        for (const auto &j: footprints[i]) {
+//            outFile << j.node_id << " ";
+//        }
+        outFile << endl;
     }
-
-    return v;
+    outFile.close();
 }
 
-vector<Request> createRequests(
-        RoadNetwork &rN, Coord sCenter, Coord tCenter, int r1, int r2)
-{
-    vector<Request> requestODs;
-    vector<NodeId> sources = getNodesInR(sCenter, r1, rN);
-    vector<NodeId> targets = getNodesInR(tCenter, r2, rN);
+vector<vector<NodeId>> runSVPBaseline(RoadNetwork &rN, vector<Request> &requestODs, int k, int threadNum, int capThres) {
+    Baseline baseline(rN, threadNum, capThres);
+    baseline.getRequests(requestODs);
 
-    int i = 0;
-    for (unsigned int &source: sources)
-    {
-        for (unsigned int &target: targets)
-        {
-            requestODs.emplace_back(Request(i, source, target, 0));
-            i++;
-        }
-    }
-    return requestODs;
+    baseline.getUniqODs();
+
+    vector<vector<NodeId>> pathResults;
+    Timer clock;
+    clock.tick();
+    baseline.svpBaseLine(pathResults, k, 0.75);
+
+    long int cost = baseline.evaluate(pathResults);
+
+    clock.tock();
+    cout << clock.duration().count()/1000 << "\t" << cost << endl;
+
+    return pathResults;
 }
 
-vector<Request> createRequests(
-        RoadNetwork &rN, NodeId v1, NodeId v2, int r1, int r2)
-{
-    vector<Request> requestODs;
-    vector<NodeId> sources = getNodesInR(rN.coords[v1], r1, rN);
-    vector<NodeId> targets = getNodesInR(rN.coords[v2], r2, rN);
-    int i;
-    for (unsigned int &source: sources)
-    {
-        for (unsigned int &target: targets)
-        {
-            requestODs.emplace_back(Request(i, source, target, 0));
-            i++;
-        }
-    }
-
-    return requestODs;
-}
-
-vector<Request> createRequests(RoadNetwork &rN, const basic_string<char> &input)
-{
-    ifstream infile(input);
-    NodeId v1, v2;
-    vector<Request> requestODs;
-    int i = 0;
-    while (infile >> v1 >> v2)
-    {
-        assert(v1 < rN.numNodes && v2 < rN.numNodes);
-        if (v1 == v2)
-            continue;
-        requestODs.emplace_back(Request(i, v1, v2, 0));
-        i++;
-    }
-
-    return requestODs;
-}
-
-vector<Path> writeEsxPaths(TrafficMaintain &traffic, int k)
-{
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    vector<Path> paths;
-    for (const auto &od: traffic.requestODs)
-    {
-        vector<Path> vP = esx(&traffic.rN, od.o, od.d, k, 0.75);
-    }
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout << "Esx consumes " << chrono::duration_cast<chrono::seconds>(end - begin).count()
-         << "[s] for " << traffic.reqNo << " ods" << endl;
-
-    begin = chrono::steady_clock::now();
-    for (const auto &od: traffic.requestODs)
-    {
-        esx_complete(&traffic.rN, od.o, od.d, k, 0.75);
-    }
-    end = chrono::steady_clock::now();
-    cout << "Esx_c consumes " << chrono::duration_cast<chrono::seconds>(end - begin).count()
-         << "[s] for " << traffic.reqNo << " ods" << endl;
-
-    begin = chrono::steady_clock::now();
-    for (const auto &od: traffic.requestODs)
-    {
-        svp_plus(&traffic.rN, od.o, od.d, k, 0.75);
-    }
-    end = chrono::steady_clock::now();
-    cout << "svp_plus consumes " << chrono::duration_cast<chrono::seconds>(end - begin).count()
-         << "[s] for " << traffic.reqNo << " ods" << endl;
-
-    begin = chrono::steady_clock::now();
-    for (const auto &od: traffic.requestODs)
-    {
-        svp_plus_complete(&traffic.rN, od.o, od.d, k, 0.75);
-    }
-    end = chrono::steady_clock::now();
-    cout << "svp_plus_c consumes " << chrono::duration_cast<chrono::seconds>(end - begin).count()
-         << "[s] for " << traffic.reqNo << " ods" << endl;
-
-    return paths;
-}
-
-void runBaseLine(RoadNetwork &rN, vector<Request> &requestODs, int k, double theta,
-                 int timeIntNum, int timeResolution, int penalR, int threadNum, double threshold)
-{
-    TrafficMaintain traffic(rN, requestODs, timeIntNum, timeResolution, penalR, threadNum, threshold);
-
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    KSPAlloc kspAlloc(traffic, k, theta);
-    kspAlloc.assignPath();
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout << "random allocation cost: " << kspAlloc.getCost() << endl;
-    cout << "baseline consumes [s]: " << chrono::duration_cast<chrono::seconds>(end - begin).count() << endl;
-}
-
-void runBaseLine(RoadNetwork &rN, Coord &sc, Coord &tc, int k, int r, double theta,
-                 int timeIntNum, int timeResolution, int penalR, int threadNum, double threshold)
-{
-    vector<Request> requestODs = createRequests(rN, sc, tc, r, r);
-    cout << "\nreqNo: " << requestODs.size() << endl;
-    runBaseLine(rN, requestODs, k, theta, timeIntNum, timeResolution, penalR, threadNum, threshold);
-}
-
-void runBaseLine(RoadNetwork &rN, pair<NodeId, NodeId> seedOd, int k, int r, double theta,
-                 int timeIntNum, int timeResolution, int penalR, int threadNum, int threshold)
-{
-    vector<Request> requestODs = createRequests(rN, seedOd.first, seedOd.second, r, r);
-    if (requestODs.size() > 4000)
-        return;
-    cout << "\nreqNo: " << requestODs.size() << endl;
-    runBaseLine(rN, requestODs, k, theta, timeIntNum, timeResolution, penalR, threadNum, threshold);
-}
-
-void runMyAlg(RoadNetwork &rN, vector<Request> &requestODs,
-              int timeIntNum, int timeResolution, int penalR, double frac, bool fix, int threadNum, double threshold)
-{
-    TrafficMaintain traffic(rN, requestODs, timeIntNum, timeResolution, penalR, threadNum, threshold);
-
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    routingMain(traffic, frac, fix);
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-    cout << "time difference[s] = " << chrono::duration_cast<chrono::seconds>(end - begin).count() << endl;
+TrafficMaintain runBaselineAllSP(RoadNetwork &rN, vector<Request> &requestODs, int threadNum, int capThres) {
+//    cout << "AllSP" << endl;
+    TrafficMaintain traffic(rN, requestODs, threadNum, capThres);
     vector<RequestId> reqs;
     boost::push_back(reqs, boost::irange(0, traffic.reqNo));
-    traffic.deleteLabels(reqs);
+    long int optimalCost = 0;
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    Timer clock;
+    clock.tick();
+    Framework::allDij(traffic, reqs, optimalCost);
+    clock.tock();
+    long int t1 = clock.duration().count();
+
+    clock.tick();
+    long int trafficCost = Framework::simulateTrafficNoCong(traffic);
+    clock.tock();
+    long int t2 = clock.duration().count();
+
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "\t" << t1 / 1000 << "\t" << t2 / 1000
+         << "\t" << optimalCost << "\t" << trafficCost << endl;
+
+    return traffic;
 }
 
-void runMyAlg(RoadNetwork &rN, pair<NodeId, NodeId> &od, int timeIntNum, int timeResolution,
-              int penalR, int r, double frac, bool fix, int threadNum, double threshold)
-{
-    vector<Request> requestODs = createRequests(rN, od.first, od.second, r, r);
-    if (requestODs.size() > 4000)
-        return;
-    cout << "\nreqNo: " << requestODs.size() << endl;
-    runMyAlg(rN, requestODs, timeIntNum, timeResolution, penalR, frac, fix, threadNum, threshold);
+TrafficMaintain runLinear(RoadNetwork &rN, vector<Request> &requestODs, int threadNum, int capThres) {
+//    cout << "Linear" << endl;
+    TrafficMaintain traffic(rN, requestODs, threadNum, capThres);
+    MainAlg::greedyMain(traffic);
+
+    return traffic;
 }
 
-void runMyAlg(RoadNetwork &rN, Coord &o, Coord &d, int timeIntNum, int timeResolution,
-              int penalR, int r, double frac, bool fix, int threadNum, double threshold)
-{
-    vector<Request> requestODs = createRequests(rN, o, d, r, r);
-    cout << "\nreqNo: " << requestODs.size() << endl;
-    runMyAlg(rN, requestODs, timeIntNum, timeResolution, penalR, frac, fix, threadNum, threshold);
+TrafficMaintain
+runBatch(RoadNetwork &rN, vector<Request> &requestODs, int threadNum, float sigma, float tau, int capThres) {
+//    cout << "Batch" << " sigma:" << sigma << " tau:" << tau << endl;
+
+    TrafficMaintain traffic(rN, requestODs, threadNum, capThres);
+    MainAlg::batchMain(traffic, sigma, tau);
+
+    return traffic;
 }
 
-vector<pair<NodeId, NodeId>> readSeedOds(const basic_string<char> &seedOdPath)
-{
-    ifstream infile(seedOdPath);
-    int lnode, rnode;
-    vector<pair<NodeId, NodeId>> seedOds;
-    while (infile >> lnode >> rnode)
-    {
-        seedOds.emplace_back(make_pair(lnode, rnode));
+TrafficMaintain runBatchIter(
+        RoadNetwork &rN, vector<Request> &requestODs, int threadNum, float sigma, float tau, float frac, int capThres) {
+    cout << "BatchIter" << " sigma:" << sigma << " tau:" << tau << " fra:" << frac << endl;
+    TrafficMaintain traffic(rN, requestODs, threadNum, capThres);
+    MainAlg::batchMainIter(traffic, sigma, tau, frac);
+
+    return traffic;
+}
+
+void readVaryDisQsDirectly(
+        string &basepath, RoadNetwork &rN,
+        int threadNum, float sigma, float tau, float frac, int capThres, int rep) {
+
+    /**
+    IJCAI ijcai(rN);
+    Timer clock;
+    clock.tick();
+    ijcai.allDij(threadNum);
+    clock.tock();
+    cout << "AllDij Time:" << clock.duration().count() / 1000 << endl;
+    **/
+
+    vector<string> distances{"5", "10", "20", "40"};
+    for (int i = 0; i < distances.size(); i++) {
+        for (int j = 0; j < 15; j++) {
+            string path = basepath + "input3/dist" + distances[i] + "km_" + to_string(j) + ".txt";
+
+            int o, d, departT;
+            vector<Request> reqs;
+            ifstream infile(path);
+            while (infile >> o >> d >> departT) {
+                assert(o <= rN.numNodes && d <= rN.numNodes);
+                for (int k = 0; k < rep; k++) {
+                    reqs.emplace_back(reqs.size(), o, d, departT);
+                }
+
+            }
+            if (reqs.empty()) {
+                cout << "No request in " << j << endl;
+                continue;
+            }
+            /**
+            ijcai.getRequests(requests);
+            vector<NodeId> nodeSeq;
+            vector<vector<NodeId>> paths(requests.size(), nodeSeq);
+            clock.tick();
+            ijcai.greedyExpansion(capThres,paths);
+            clock.tock();
+            long int trafficCost = ijcai.evaluate(capThres, paths);
+
+            cout << clock.duration().count()/1000 << "\t" << trafficCost << endl;
+            **/
+//            runBaselineAllSP(rN, reqs, threadNum, capThres);
+            TrafficMaintain traffic = runBaselineAllSP(rN, reqs, threadNum, capThres);
+//            int candiPNum = 10;
+//            vector<vector<NodeId>> paths = runSVPBaseline(rN, reqs, candiPNum, threadNum, capThres);
+//            TrafficMaintain traffic = runLinear(rN, requests, threadNum, capThres);
+//            TrafficMaintain traffic = runBatch(rN, reqs, threadNum, sigma, tau, capThres);
+//            TrafficMaintain traffic = runBatchIter(rN, requests, threadNum, sigma, tau, frac, capThres);
+        }
+    }
+}
+
+
+vector<Request> readRealLifeData(
+        string &basepath, RoadNetwork &rN,
+        int threadNum, float sigma, float tau, float frac, int capThres) {
+    int o, d, departT;
+    vector<Request> reqs;
+    ifstream infile(basepath);
+    while (infile >> o >> d >> departT) {
+        assert(o <= rN.numNodes && d <= rN.numNodes);
+        reqs.emplace_back(reqs.size(), o, d, departT);
+    }
+    infile.close();
+    return reqs;
+}
+
+int main() {
+
+    string basepath1 = "/export/project/yehong/traffic-assignment/data/";
+    string basepath2 = "/Users/xyh/Desktop/traffic-assignment/data/";
+    string basepath3 = "/home/yxudi/GRO/data/";
+
+    string basepath = basepath3, location = "BJ";
+    int threadNum = 160, capThres = 2, rep = 1; // c <=2 15%: ; c <= 5: 26%; c <= 10: 41%; c <= 20: 61%
+    float frac = 0.005, sigma = 250, tau = 0.05;
+
+    string map, coords, realLifePath, varyDistPath, sdPath = basepath + location + "_allSDs.txt";
+    vector<string> hours;
+    RoadNetwork rN;
+    if (location == "NY" or location == "Wuhan") {
+        map = basepath + location + ".txt", coords = basepath + "NY_NodeIDLonLat.txt";
+        rN.readGraph(map, coords, 2);
+        hours = {"7x5", "67x3", "678x2"};
+        realLifePath = basepath + "real_life/NY/input/req";
+
+    } else if (location == "BJ") {
+        map = basepath + "BJ_map.txt", coords = basepath + "BJ_NodeIDLonLat.txt";
+        rN.readGraph(map, coords, 1);
+        varyDistPath = basepath + "extreme_congested/";
+        hours = {"6x4", "6x3&78x1", "678x2"};
+        realLifePath = basepath + "real_life/BJ/input/";
     }
 
-    return seedOds;
-}
 
-int main()
-{
-    string basepath1 = "/media/TraminerData/yehong_ziyi/global_routing/input/";
-    string basepath2 = "/media/bigdata/s4451682/Yehong/";
-    string basepath3 = "/Users/xyh/Desktop/traffic-assignment/data/";
+//    for (const auto &i: {1, 2, 4}) {
+//        rep = i;
+//        cout << "BAT rep: " << rep  << endl;
+//        readVaryDisQsDirectly(varyDistPath, rN, threadNum, sigma, tau, frac, capThres, rep);
+//    }
 
-    string basepath = basepath3;
-    string map = basepath + "BJ_map.txt";
-    string coords = basepath + "BJ_NodeIDLonLat.txt";
+//    /**
+    Baseline baseline(rN, threadNum, capThres);
+    Timer clock;
+    clock.tick();
+    vector<vector<int>> distances(rN.numNodes + 1, vector<int>(rN.numNodes + 1, 0));
+    baseline.allDij(distances);
+    clock.tock();
+    cout << location << " AllDij Time:" << clock.duration().count() / 1000 << endl;
+//    **/
 
-    string queryset = "seedod50";
-    string inputSeedOd = basepath + queryset + ".txt";
-    int r = 500, penalR = 5, timeResolution = 600, timeIntNum = 2000, threadNum = 50;
-    double frac = 0.3, threshold = 0.67;
-    bool fix = false;
-    RoadNetwork rN(map.c_str(), coords.c_str());
-    cout << "query: " << queryset << " r: " << r << " threshold: " << threshold << " frac: " << "threadNum: " << threadNum <<
-         frac << " fix:" << fix << " timeIntNum: " << timeIntNum << " timeResolution: " << timeResolution << endl;
-
-    vector<pair<NodeId, NodeId>> seedODs = readSeedOds(inputSeedOd);
-
-    for (auto &seedOD: seedODs)
-    {
-        runMyAlg(rN, seedOD, timeIntNum, timeResolution, penalR, r, frac, fix, threadNum, threshold);
-//        runBaseLine(rN, seedOD, 10, r, 0.75, timeIntNum, timeResolution, penalR, threadNum);
+//    /**
+    for (auto const &hour: hours) {
+        int numCandi = 10;
+        cout << "B3 dataset: " << hour << endl;
+        string realLifeD = realLifePath + hour + ".txt";
+        vector<Request> requests = readRealLifeData(realLifeD, rN, threadNum, sigma, tau, frac, capThres);
+        assert(!requests.empty());
+//        vector<vector<NodeId>> paths = runSVPBaseline(rN, requests, numCandi, threadNum, capThres);
+        vector<vector<NodeId>> paths(requests.size(), vector<NodeId>());
+        clock.tick();
+        baseline.greedyExpansion(distances, paths);
+        clock.tock();
+        long int cost = baseline.evaluate(paths);
+        cout << clock.duration().count() / 1000 << "\t" << cost << endl;
+//        for (const auto &sig: {8000, 16000, 32000, 64000}) {
+//            sigma = sig;
+//            for (const auto &reFrac: {0.0005, 0.001, 0.01}) {
+//                frac = reFrac;
+//                runBatchIter(rN, requests, threadNum, sigma, tau, frac, capThres);
+//            }
+//        }
     }
-
-//    Coord sc(116.207, 40.091), tc(116.518, 39.8754);
-//    runMyAlg(rN, sc, tc, timeIntNum, timeResolution, penalR, r, frac, fix, threadNum, threshold);
-
-//    runBaseLine(rN, sc, tc, 10, r, 0.75, timeIntNum, timeResolution, penalR, threadNum, threshold);
+//    **/
     cout << "done" << endl;
     return 0;
 }
